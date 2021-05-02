5df5c914dfca0f6219727e0d73a4cdedc8f78c08
tobicorradi <corraditobias@gmail.com>
2021-05-02 13:58:21 -0300

    PaymentBlock styling

--- src/components/PaymentBlock/PaymentBlock.jsx
+++ src/components/PaymentBlock/PaymentBlock.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import PaymentItem from "./../PaymentItem";
+import { ReactComponent as IconCredit } from "./../../assets/img/iconCredit.svg";
+import $ from "styled-components";
+const $PaymentBlock = $.div`
+  background-color: white;
+  padding: 20px;
+`;
 const PaymentBlock = () => {
   return (
-    <div>
+    <$PaymentBlock className="payment-block">
       <PaymentItem
+        icon={<IconCredit />}
         text={"Tarjeta de Crédito"}
         link={"Ver promociones bancarias"}
       />
       <PaymentItem
+        icon={<IconCredit />}
         text={"Tarjeta de débito"}
         link={"Ver más"}
       />
-      <PaymentItem
-        text={"Efectivo"}
-        link={"Ver más"}
-      />
-    </div>
+      <PaymentItem 
+        icon={<IconCredit />} 
+        text={"Efectivo"} 
+        link={"Ver más"} />
+    </$PaymentBlock>
   );
 };
 
