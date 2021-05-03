900185ff594fb511bd2c41b51f8a791f2d87ecbf
tobicorradi <corraditobias@gmail.com>
2021-05-02 23:22:37 -0300

    Padding added to Container component

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -4,6 +4,7 @@ import $ from "styled-components";
 const $Container = $.div`
   max-width: 1200px;
   margin: 0 auto;
+  padding: 0 30px;
 `;
 const Container = ({ children }) => {
   return <$Container className="container">{children}</$Container>;
