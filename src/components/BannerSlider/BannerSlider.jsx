28ee2b44b250040ad15598fd275c8d313a894ba4
tobicorradi <corraditobias@gmail.com>
2021-05-03 19:30:34 -0300

    Images added to slider

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -24,19 +24,19 @@ const BannerSlider = () => {
       </div>
       <div>
         <$Item
-          src="https://http2.mlstatic.com/D_NQ_797135-MLA45643423773_042021-OO.webp"
+          src="https://http2.mlstatic.com/D_NQ_765446-MLA45793899011_052021-OO.webp"
           alt=""
         />
       </div>
       <div>
         <$Item
-          src="https://http2.mlstatic.com/D_NQ_797135-MLA45643423773_042021-OO.webp"
+          src="https://http2.mlstatic.com/D_NQ_881096-MLA45793115543_052021-OO.webp"
           alt=""
         />
       </div>
       <div>
         <$Item
-          src="https://http2.mlstatic.com/D_NQ_797135-MLA45643423773_042021-OO.webp"
+          src="https://http2.mlstatic.com/D_NQ_754007-MLA45757279995_042021-OO.webp"
           alt=""
         />
       </div>
