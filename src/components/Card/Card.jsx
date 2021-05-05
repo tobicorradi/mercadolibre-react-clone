8a2b55afd38802a1ee17f8bbf2a543faac25a087
tobicorradi <corraditobias@gmail.com>
2021-05-05 14:49:02 -0300

    Props added to Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({icon}) => {
-    return (
-        <div>
-            {icon}
-        </div>
-    )
-}
+const Card = ({ icon, title, text, link }) => {
+  return (
+    <div>
+      <div className="">{icon}</div>
+      <div>
+        <h4>{title}</h4>
+        <p>{text}</p>
+        <a href="">{link}</a>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
